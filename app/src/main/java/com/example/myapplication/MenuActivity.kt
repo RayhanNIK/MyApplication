43f9531b5ef4e552634078f3f52366b87209de21
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-05-03 16:33:28 +0700

    Menu - Add button to open Lifecycle Activity

--- app/src/main/java/com/example/myapplication/MenuActivity.kt
+++ app/src/main/java/com/example/myapplication/MenuActivity.kt
@@ -1,11 +1,25 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class MenuActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+
+        val btnLifecycle: Button = findViewById(R.id.btnLifecycle)
+        btnLifecycle.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        Log.d("Log intent", "Intent")
+        val lifecycleIntent = Intent(this@MenuActivity, LifecycleActivity::class.java)
+        startActivity(lifecycleIntent)
     }
 }
