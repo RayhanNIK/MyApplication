e075700b8496598c990e16fef250bae76cf77eb0
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-05-03 16:47:20 +0700

    Menu - Add condition to open Main and Lifecycle activity

--- app/src/main/java/com/example/myapplication/MenuActivity.kt
+++ app/src/main/java/com/example/myapplication/MenuActivity.kt
@@ -12,14 +12,25 @@ class MenuActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
+        val btnHello: Button = findViewById(R.id.btnHello)
+        btnHello.setOnClickListener(this)
 
         val btnLifecycle: Button = findViewById(R.id.btnLifecycle)
         btnLifecycle.setOnClickListener(this)
     }
 
-    override fun onClick(v: View?) {
-        Log.d("Log intent", "Intent")
-        val lifecycleIntent = Intent(this@MenuActivity, LifecycleActivity::class.java)
-        startActivity(lifecycleIntent)
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.btnHello -> {
+                Log.d("Log intent", "Intent Hello")
+                val helloIntent = Intent(this@MenuActivity, MainActivity::class.java)
+                startActivity(helloIntent)
+            }
+            R.id.btnLifecycle -> {
+                Log.d("Log intent", "Intent")
+                val lifecycleIntent = Intent(this@MenuActivity, LifecycleActivity::class.java)
+                startActivity(lifecycleIntent)
+            }
+        }
     }
 }
