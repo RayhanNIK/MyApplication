3602fa58e722dfe6449e188752856bd37e65f2ef
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-05-10 18:24:35 +0700

    Intent - Initialize button Map in Activity

--- app/src/main/java/com/example/myapplication/IntentActivity.kt
+++ app/src/main/java/com/example/myapplication/IntentActivity.kt
@@ -1,11 +1,18 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class IntentActivity : AppCompatActivity() {
+class IntentActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent)
+
+        val btnMap: Button = findViewById(R.id.btnMap)
+        btnMap.setOnClickListener(this)
     }
 }
+
+
