af7f08bd6074acc5e282dcbe08e5e41029e12bb2
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-05-10 18:43:23 +0700

    Main - Add OnClick listener for Intent button

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,12 +11,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val myText: TextView = findViewById(R.id.myText1)
         myText.text = "Goodbye"
 
         val myButton: Button = findViewById(R.id.myButton1)
         myButton.setOnClickListener(this)
+
+        val btnIntent: Button = findViewById(R.id.btnIntent)
+        btnIntent.setOnClickListener(this)
         }
+
+
 
     override fun onClick(v: View?) {
         Toast.makeText(this,"Click", Toast.LENGTH_SHORT).show()
