36a89da75c8b080d515e1561e378434143859bb5
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-06-14 13:29:05 +0700

    Operation - Initialize all views

--- app/src/main/java/com/example/myapplication/OperationActivity.kt
+++ app/src/main/java/com/example/myapplication/OperationActivity.kt
@@ -1,11 +1,36 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class OperationActivity : AppCompatActivity() {
+
+    private lateinit var etBilanganX: EditText
+    private lateinit var etBilanganY: EditText
+    private lateinit var tvOperator: TextView
+    private lateinit var tvOperationResult: TextView
+    private lateinit var btnOpAdd: Button
+    private lateinit var btnOpSubstract: Button
+    private lateinit var btnOpMultiply: Button
+    private lateinit var btnOpDivide: Button
+    private lateinit var btnOpReset: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_operation)
+
+        etBilanganX = findViewById(R.id.etBilanganX)
+        etBilanganY = findViewById(R.id.etBilanganY)
+        tvOperator = findViewById(R.id.tvOperator)
+        tvOperationResult = findViewById(R.id.tvOperationResult)
+        btnOpAdd = findViewById(R.id.btnOpAdd)
+        btnOpSubstract = findViewById(R.id.btnOpSubstract)
+        btnOpMultiply = findViewById(R.id.btnOpMultiply)
+        btnOpDivide = findViewById(R.id.btnOpDivide)
+        btnOpReset = findViewById(R.id.btnOpReset)
+
     }
 }
