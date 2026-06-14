17f39171c6950ea46303b40ff84198afcd85d108
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-06-14 12:38:40 +0700

    Volume - initialize views

--- app/src/main/java/com/example/myapplication/VolumeActivity.kt
+++ app/src/main/java/com/example/myapplication/VolumeActivity.kt
@@ -1,11 +1,28 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class VolumeActivity : AppCompatActivity() {
+
+    private lateinit var edtLength: EditText
+    private lateinit var edtWidth: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_volume)
+
+        edtLength = findViewById(R.id.edtLength)
+        edtWidth = findViewById(R.id.edtWidth)
+        edtHeight = findViewById(R.id.edtHeight)
+        btnCalculate = findViewById(R.id.btnCalculate)
+        tvResult = findViewById(R.id.tvResult)
+
     }
 }
