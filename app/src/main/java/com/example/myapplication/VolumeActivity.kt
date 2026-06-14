0bf56f2c7756f40d8e9a2f41c69e895bc7d3d748
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-06-14 12:49:26 +0700

    Volume - Calculate volume from width, height, and length inputs

--- app/src/main/java/com/example/myapplication/VolumeActivity.kt
+++ app/src/main/java/com/example/myapplication/VolumeActivity.kt
@@ -1,12 +1,13 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class VolumeActivity : AppCompatActivity() {
+class VolumeActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var edtLength: EditText
     private lateinit var edtWidth: EditText
@@ -24,5 +25,29 @@ class VolumeActivity : AppCompatActivity() {
         btnCalculate = findViewById(R.id.btnCalculate)
         tvResult = findViewById(R.id.tvResult)
 
+        btnCalculate.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btnCalculate) {
+
+            val length = edtLength.text.toString().trim()
+            val width = edtWidth.text.toString().trim()
+            val height = edtHeight.text.toString().trim()
+
+            when {
+                length.isEmpty() -> edtLength.error = "Empty Field"
+                width.isEmpty() -> edtWidth.error = "Empty Field"
+                height.isEmpty() -> edtHeight.error = "Empty Field"
+
+                else -> {
+                    val volume = length.toDouble() * width.toDouble() * height.toDouble()
+                    tvResult.text = volume.toString()
+                }
+
+            }
+
+        }
     }
 }
