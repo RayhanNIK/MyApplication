ce10649b636af2d606a364bccaac2ca1ac1bf958
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-06-27 13:18:33 +0700

    Classification - Init all views

--- app/src/main/java/com/example/myapplication/ClassificationActivity.kt
+++ app/src/main/java/com/example/myapplication/ClassificationActivity.kt
@@ -1,11 +1,23 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ClassificationActivity : AppCompatActivity() {
+
+    private lateinit var etClassificationScore: EditText
+    private lateinit var btnClassificationShow: Button
+    private lateinit var tvClassificationResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_classification)
+
+        etClassificationScore = findViewById(R.id.etClassificationScore)
+        btnClassificationShow = findViewById(R.id.btnClassificationShow)
+        tvClassificationResult = findViewById(R.id.tvClassificationResult)
     }
 }
