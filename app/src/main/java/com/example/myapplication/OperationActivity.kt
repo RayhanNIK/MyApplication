4f4261781b5a7923d7e1e6721a4ad46d66815fdd
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-07-06 11:13:10 +0700

    Operation - Implement view binding

--- app/src/main/java/com/example/myapplication/OperationActivity.kt
+++ app/src/main/java/com/example/myapplication/OperationActivity.kt
@@ -2,85 +2,65 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityOperationBinding
 
 class OperationActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var etBilanganX: EditText
-    private lateinit var etBilanganY: EditText
-    private lateinit var tvOperator: TextView
-    private lateinit var tvOperationResult: TextView
-    private lateinit var btnOpAdd: Button
-    private lateinit var btnOpSubstract: Button
-    private lateinit var btnOpMultiply: Button
-    private lateinit var btnOpDivide: Button
-    private lateinit var btnOpReset: Button
-
+    private lateinit var binding: ActivityOperationBinding
     private var inputX: Long = 0
     private var inputY: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_operation)
+        binding = ActivityOperationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        etBilanganX = findViewById(R.id.etBilanganX)
-        etBilanganY = findViewById(R.id.etBilanganY)
-        tvOperator = findViewById(R.id.tvOperator)
-        tvOperationResult = findViewById(R.id.tvOperationResult)
-        btnOpAdd = findViewById(R.id.btnOpAdd)
-        btnOpSubstract = findViewById(R.id.btnOpSubstract)
-        btnOpMultiply = findViewById(R.id.btnOpMultiply)
-        btnOpDivide = findViewById(R.id.btnOpDivide)
-        btnOpReset = findViewById(R.id.btnOpReset)
-
-        btnOpAdd.setOnClickListener(this)
-        btnOpSubstract.setOnClickListener(this)
-        btnOpMultiply.setOnClickListener(this)
-        btnOpDivide.setOnClickListener(this)
-        btnOpReset.setOnClickListener(this)
+        binding.btnOpAdd.setOnClickListener(this)
+        binding.btnOpSubstract.setOnClickListener(this)
+        binding.btnOpMultiply.setOnClickListener(this)
+        binding.btnOpDivide.setOnClickListener(this)
+        binding.btnOpReset.setOnClickListener(this)
     }
 
     private fun getInputNumbers() {
-        if (etBilanganX.text.isNotEmpty() == true || etBilanganY.text.isNotEmpty() == true) {
-            inputX = etBilanganX.text.toString().toLong()
-            inputY = etBilanganY.text.toString().toLong()
+        if (binding.etBilanganX.text?.isNotEmpty() == true || binding.etBilanganY.text?.isNotEmpty() == true) {
+            inputX = binding.etBilanganX.text.toString().toLong()
+            inputY = binding.etBilanganY.text.toString().toLong()
         }
     }
 
     override fun onClick(view: View) {
         when(view.id) {
             R.id.btnOpAdd -> {
-                tvOperator.text = resources.getString(R.string.operation_add)
+                binding.tvOperator.text = resources.getString(R.string.operation_add)
                 getInputNumbers()
                 val operation = inputX + inputY
-                tvOperationResult.text = operation.toString()
+                binding.tvOperationResult.text = operation.toString()
             }
             R.id.btnOpSubstract -> {
-                tvOperator.text = resources.getString(R.string.operation_substract)
+                binding.tvOperator.text = resources.getString(R.string.operation_substract)
                 getInputNumbers()
                 val operation = inputX - inputY
-                tvOperationResult.text = operation.toString()
+                binding.tvOperationResult.text = operation.toString()
             }
             R.id.btnOpMultiply -> {
-                tvOperator.text = resources.getString(R.string.operation_multiply)
+                binding.tvOperator.text = resources.getString(R.string.operation_multiply)
                 getInputNumbers()
                 val operation = inputX * inputY
-                tvOperationResult.text = operation.toString()
+                binding.tvOperationResult.text = operation.toString()
             }
             R.id.btnOpDivide -> {
-                tvOperator.text = resources.getString(R.string.operation_divide)
+                binding.tvOperator.text = resources.getString(R.string.operation_divide)
                 getInputNumbers()
                 val operation = inputX / inputY
-                tvOperationResult.text = operation.toString()
+                binding.tvOperationResult.text = operation.toString()
             }
             R.id.btnOpReset -> {
-                etBilanganX.setText("0")
-                etBilanganY.setText("0")
-                tvOperator.text = ""
-                tvOperationResult.text = "0"
+                binding.etBilanganX.setText("0")
+                binding.etBilanganY.setText("0")
+                binding.tvOperator.text = ""
+                binding.tvOperationResult.text = "0"
             }
         }
     }
