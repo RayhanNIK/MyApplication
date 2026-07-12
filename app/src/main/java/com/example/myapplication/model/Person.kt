56eb5e964c23e26e004aa8cc60e4f8dbe0e01f0e
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-07-12 11:54:28 +0700

    Model - Implement Parcelable in person data class

--- app/src/main/java/com/example/myapplication/model/Person.kt
+++ app/src/main/java/com/example/myapplication/model/Person.kt
@@ -1,10 +1,21 @@
 package com.example.myapplication.model
 
+import android.annotation.SuppressLint
+import android.os.Parcel
 import android.os.Parcelable
 
+@SuppressLint("ParcelCreator")
 data class Person (
     var name: String?,
     var age: Int?,
     var email: String?,
     var city: String?
-) : Parcelable
+) : Parcelable {
+    override fun describeContents(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        TODO("Not yet implemented")
+    }
+}
