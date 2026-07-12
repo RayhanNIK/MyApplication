b051fca6af32295c7f9240548dea668427a8f1e3
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-07-12 11:25:09 +0700

    IntentMoveData - Get intent extra of name and age

--- app/src/main/java/com/example/myapplication/IntentMoveDataActivity.kt
+++ app/src/main/java/com/example/myapplication/IntentMoveDataActivity.kt
@@ -6,11 +6,21 @@ import com.example.myapplication.databinding.ActivityIntentMoveDataBinding
 
 class IntentMoveDataActivity : AppCompatActivity() {
 
+    companion object {
+        const val EXTRA_AGE = "extra_age"
+        const val EXTRA_NAME = "extra_name"
+    }
+
     private lateinit var binding: ActivityIntentMoveDataBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityIntentMoveDataBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val name = intent.getStringExtra(EXTRA_NAME)
+        val age = intent.getIntExtra(EXTRA_AGE,0)
+        val data = "Nama : $name, Age : $age"
+        binding.tvDataReceived.setText(data)
     }
 }
