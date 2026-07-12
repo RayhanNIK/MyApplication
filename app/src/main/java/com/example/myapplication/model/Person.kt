2a6e1f5b0d0fa253246cf1568e7c36f8ad2e9c6d
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-07-12 11:57:39 +0700

    Person - add constructor and companion object

--- app/src/main/java/com/example/myapplication/model/Person.kt
+++ app/src/main/java/com/example/myapplication/model/Person.kt
@@ -11,11 +11,23 @@ data class Person (
     var email: String?,
     var city: String?
 ) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readString(),
+        parcel.readString()) {
+    }
+
     override fun describeContents(): Int {
         TODO("Not yet implemented")
     }
 
     override fun writeToParcel(dest: Parcel, flags: Int) {
         TODO("Not yet implemented")
+    }
+
+    companion object CREATOR : Parcelable.Creator<Person> {
+        
     }
 }
