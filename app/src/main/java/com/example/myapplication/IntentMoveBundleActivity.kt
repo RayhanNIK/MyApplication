30e841ecfa06e12911451949e359d527122d397c
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-07-19 20:35:45 +0700

    IntentMoveBundle - Receive intent extra from bundle

--- app/src/main/java/com/example/myapplication/IntentMoveBundleActivity.kt
+++ app/src/main/java/com/example/myapplication/IntentMoveBundleActivity.kt
@@ -8,9 +8,30 @@ class IntentMoveBundleActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityIntentMoveBundleBinding
 
+    companion object {
+        const val EXTRA_BUNDLE_AGE = "extra_age"
+        const val EXTRA_BUNDLE_NAME = "extra_name"
+    }
+
+    private var name: String = ""
+    private var age: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityIntentMoveBundleBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        getIntentExtras()
+        showData()
+    }
+
+    private fun getIntentExtras() {
+        name = intent.extras?.getString(EXTRA_BUNDLE_NAME) ?: ""
+        age = intent.extras?.getInt(EXTRA_BUNDLE_AGE, 0) ?: 0
+    }
+
+    private fun showData() {
+        val text = "Nama : $name, Age : $age"
+        binding.tvBundleReceived.text = text
     }
 }
