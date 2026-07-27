e3fbfc88868dcfc1c2545101bb47193cf315f183
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-07-27 16:43:30 +0700

    IntentMoveResult - Send RadioButton value as intent result

--- app/src/main/java/com/example/myapplication/IntentMoveResultActivity.kt
+++ app/src/main/java/com/example/myapplication/IntentMoveResultActivity.kt
@@ -1,11 +1,17 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityIntentMoveResultBinding
 
 class IntentMoveResultActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        const val EXTRA_VALUE = "extra_value"
+        const val RESULT_CODE = 110
+    }
 
     private lateinit var binding: ActivityIntentMoveResultBinding
 
@@ -19,7 +25,20 @@ class IntentMoveResultActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(view: View) {
         if (view == binding.btnResultChoose) {
+            if (binding.rgNumber.checkedRadioButtonId != 0) {
+                var value = 0
+                when (binding.rgNumber.checkedRadioButtonId) {
+                    R.id.rb50 -> value = 50
+                    R.id.rb100 -> value = 100
+                    R.id.rb150 -> value = 150
+                    R.id.rb200 -> value = 200
+                }
 
+                val resultIntent = Intent()
+                resultIntent.putExtra(EXTRA_VALUE, value)
+                setResult(RESULT_CODE, resultIntent)
+                finish()
+            }
         }
     }
 }
