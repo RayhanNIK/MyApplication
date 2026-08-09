2e3c4d99976ce663ba3d94d9891fd56b8108b95d
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-08-09 17:09:10 +0700

    List Fruit - implement view holder functions

--- app/src/main/java/com/example/myapplication/listFruit/ListFruitAdapter.kt
+++ app/src/main/java/com/example/myapplication/listFruit/ListFruitAdapter.kt
@@ -1,8 +1,27 @@
 package com.example.myapplication.listFruit
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class ListFruitAdapter: RecyclerView.Adapter<ListFruitAdapter.ListFruitViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ListFruitViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(
+        holder: ListFruitViewHolder,
+        position: Int
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
     class ListFruitViewHolder {
 
     }
