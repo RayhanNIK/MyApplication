c36f08621e7d51c9d6f6ef569f9088e0e15d0dcf
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-08-09 17:30:29 +0700

    List Fruit - implement view binding in adapter

--- app/src/main/java/com/example/myapplication/listFruit/ListFruitAdapter.kt
+++ app/src/main/java/com/example/myapplication/listFruit/ListFruitAdapter.kt
@@ -1,23 +1,23 @@
 package com.example.myapplication.listFruit
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.R
+import com.example.myapplication.databinding.ItemListFruitBinding
 import com.example.myapplication.model.Fruit
 
 class ListFruitAdapter(
     private val itemList: List<Fruit>
 ) : RecyclerView.Adapter<ListFruitAdapter.ListFruitViewHolder>() {
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): ListFruitViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_list_fruit, parent, false)
-        return ListFruitViewHolder(view)
+        val binding = ItemListFruitBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return ListFruitViewHolder(binding)
     }
 
     override fun onBindViewHolder(
@@ -25,12 +25,11 @@ class ListFruitAdapter(
         position: Int
     ) {
         val item = itemList[position]
-        holder.tvFruitName.text = item.name
+        holder.binding.tvFruitName.text = item.name
     }
 
     override fun getItemCount(): Int = itemList.size
 
-    class ListFruitViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tvFruitName: TextView = itemView.findViewById(R.id.tvFruitName)
-    }
+    class ListFruitViewHolder(var binding: ItemListFruitBinding) :
+        RecyclerView.ViewHolder(binding.root)
 }
