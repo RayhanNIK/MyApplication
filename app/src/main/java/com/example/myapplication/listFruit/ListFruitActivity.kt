3d47f0fcc17e118aac546aedc0313058dbc96d8e
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-08-09 17:17:32 +0700

    List Fruit - set up recyclerView with its adapter

--- app/src/main/java/com/example/myapplication/listFruit/ListFruitActivity.kt
+++ app/src/main/java/com/example/myapplication/listFruit/ListFruitActivity.kt
@@ -2,11 +2,22 @@ package com.example.myapplication.listFruit
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
+import com.example.myapplication.model.FruitRepository
 
 class ListFruitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_fruit)
+
+        val repository = FruitRepository()
+        val fruitList = repository.getFruits()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rvListFruit)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        recyclerView.adapter = ListFruitAdapter(fruitList)
     }
 }
