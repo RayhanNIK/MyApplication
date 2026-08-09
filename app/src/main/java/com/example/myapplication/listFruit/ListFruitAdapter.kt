8d02ce6d9a18b7fb40f1a4f040db1ed8cb64dfba
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-08-09 17:08:04 +0700

    List Fruit - add an adapter

---
+++ app/src/main/java/com/example/myapplication/listFruit/ListFruitAdapter.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.listFruit
+
+import androidx.recyclerview.widget.RecyclerView
+
+class ListFruitAdapter: RecyclerView.Adapter<ListFruitAdapter.ListFruitViewHolder>() {
+    class ListFruitViewHolder {
+
+    }
+
+}
