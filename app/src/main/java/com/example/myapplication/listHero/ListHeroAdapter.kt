60001874b1d06c1f2fb94dc73e2be8a14fe1f459
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-08-17 11:40:35 +0700

    List Hero - Implemenet view holder function

--- app/src/main/java/com/example/myapplication/listHero/ListHeroAdapter.kt
+++ app/src/main/java/com/example/myapplication/listHero/ListHeroAdapter.kt
@@ -1,8 +1,28 @@
 package com.example.myapplication.listHero
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class ListHeroAdapter : RecyclerView.Adapter<ListHeroAdapter.ListHeroViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ListHeroViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(
+        holder: ListHeroViewHolder,
+        position: Int
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
     class ListHeroViewHolder {
 
     }
