fd493d65863aecd47ebb7646f320b085f79244bd
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-08-17 12:00:27 +0700

    List Hero - get list hero data

--- app/src/main/java/com/example/myapplication/listHero/ListHeroActivity.kt
+++ app/src/main/java/com/example/myapplication/listHero/ListHeroActivity.kt
@@ -19,7 +19,20 @@ class ListHeroActivity : AppCompatActivity() {
         rvHeroes = findViewById(R.id.rvListHero)
         rvHeroes.setHasFixedSize(true)
 
+        list.addAll(getListHeroes())
         showRecyclerList()
+    }
+
+    private fun getListHeroes(): ArrayList<Hero> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+        val listHero = ArrayList<Hero>()
+        for (i in dataName.indices) {
+            val hero = Hero(dataName[i], dataDescription[i], dataPhoto.getResourceId(i, -1))
+            listHero.add(hero)
+        }
+        return listHero
     }
 
     private fun showRecyclerList() {
