c88adc2e1c8a9ae750ef917b72732b64272e8e57
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-08-17 11:39:17 +0700

    List Hero - add an adapter

---
+++ app/src/main/java/com/example/myapplication/listHero/ListHeroAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.listHero
+
+import androidx.recyclerview.widget.RecyclerView
+
+class ListHeroAdapter : RecyclerView.Adapter<ListHeroAdapter.ListHeroViewHolder>() {
+    class ListHeroViewHolder {
+
+    }
+
+
+}
