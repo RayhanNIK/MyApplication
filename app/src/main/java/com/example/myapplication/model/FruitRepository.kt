84b767e873ea2860a137c82e2c286ae716c3e74e
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-08-17 11:00:31 +0700

    List Fruit - add more data in fruit repository

--- app/src/main/java/com/example/myapplication/model/FruitRepository.kt
+++ app/src/main/java/com/example/myapplication/model/FruitRepository.kt
@@ -7,7 +7,19 @@ class FruitRepository {
         return listOf(
             Fruit(1, "Apple", "Red", R.drawable.ic_launcher_foreground),
             Fruit(2, "Banana", "Yellow", R.drawable.ic_launcher_foreground),
-            Fruit(3, "Grape", "Purple", R.drawable.ic_launcher_foreground)
+            Fruit(3, "Grape", "Purple", R.drawable.ic_launcher_foreground),
+            Fruit(4, "Mango", "Green", R.drawable.ic_launcher_foreground),
+            Fruit(5, "Orange", "Orange", R.drawable.ic_launcher_foreground),
+            Fruit(6, "Peach", "Peach", R.drawable.ic_launcher_foreground),
+            Fruit(7, "Blueberry", "Blue", R.drawable.ic_launcher_foreground),
+            Fruit(8, "Blackberry", "Black", R.drawable.ic_launcher_foreground),
+            Fruit(9, "Strawberry", "Red", R.drawable.ic_launcher_foreground),
+            Fruit(10, "Plum", "Purple", R.drawable.ic_launcher_foreground),
+            Fruit(11, "Avocado", "Green", R.drawable.ic_launcher_foreground),
+            Fruit(12, "Pineapple", "Yellow", R.drawable.ic_launcher_foreground),
+            Fruit(13, "Watermelon", "Red", R.drawable.ic_launcher_foreground),
+            Fruit(14, "Papaya", "Orange", R.drawable.ic_launcher_foreground),
+            Fruit(15, "Kiwi", "Green", R.drawable.ic_launcher_foreground)
         )
     }
 }
