abfbf53b1393d8cb248ed699aba669d1bf786f74
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-08-17 11:43:29 +0700

    List Hero - inherit recycler view in hero viewholder

--- app/src/main/java/com/example/myapplication/listHero/ListHeroAdapter.kt
+++ app/src/main/java/com/example/myapplication/listHero/ListHeroAdapter.kt
@@ -1,7 +1,11 @@
 package com.example.myapplication.listHero
 
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.R
 
 class ListHeroAdapter : RecyclerView.Adapter<ListHeroAdapter.ListHeroViewHolder>() {
 
@@ -23,8 +27,10 @@ class ListHeroAdapter : RecyclerView.Adapter<ListHeroAdapter.ListHeroViewHolder>
         TODO("Not yet implemented")
     }
 
-    class ListHeroViewHolder {
-
+    class ListHeroViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val ivHeroPhoto: ImageView = itemView.findViewById(R.id.ivHeroPhoto)
+        val tvHeroName: TextView = itemView.findViewById(R.id.tvHeroName)
+        val tvHeroDesc: TextView = itemView.findViewById(R.id.tvHeroDesc)
     }
 
 
