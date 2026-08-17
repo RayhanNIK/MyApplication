ee1af6e60015ecdef128c818f073a7af27d3c5b6
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-08-17 11:07:35 +0700

    List Hero - add new activity

---
+++ app/src/main/java/com/example/myapplication/listHero/ListHeroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.listHero
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class ListHeroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_hero)
+    }
+}
