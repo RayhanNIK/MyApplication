6a14a018cdfdaf8f24b657f48b0c9a79be135d4a
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-09-08 20:47:45 +0700

    Hero Detail - add text and image in hero detail layout

--- app/src/main/java/com/example/myapplication/listHero/DetailHeroActivity.kt
+++ app/src/main/java/com/example/myapplication/listHero/DetailHeroActivity.kt
@@ -2,9 +2,15 @@ package com.example.myapplication.listHero
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityDetailHeroBinding
 
 class DetailHeroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailHeroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityDetailHeroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
