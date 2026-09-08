e6f5bdd0f29a439b246b33b67c4d6e045b7d80af
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-09-08 20:25:53 +0700

    Hero Detail - add new activity to show hero detail

---
+++ app/src/main/java/com/example/myapplication/listHero/DetailHeroActivity.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.listHero
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailHeroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
