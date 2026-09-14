fcaa94dc6df8693df76313f179708c973af5d2c6
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-09-14 17:32:06 +0700

    Webview - show toast when a webpage is load

--- app/src/main/java/com/example/myapplication/WebviewActivity.kt
+++ app/src/main/java/com/example/myapplication/WebviewActivity.kt
@@ -2,6 +2,8 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class WebviewActivity : AppCompatActivity() {
@@ -11,6 +13,14 @@ class WebviewActivity : AppCompatActivity() {
 
         val webView = findViewById<WebView>(R.id.webView)
         webView.settings.javaScriptEnabled = true
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                Toast.makeText(this@WebviewActivity,
+                    "Web Dicoding berhasil dimuat",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
         webView.loadUrl("https://www.dicoding.com")
     }
 }
