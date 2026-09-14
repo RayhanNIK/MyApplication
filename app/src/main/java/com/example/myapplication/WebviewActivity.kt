3b2cf17b5a1cdf5b1141b45b8942296903c229d6
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-09-14 17:25:15 +0700

    Webview - load a webpage using webview

--- app/src/main/java/com/example/myapplication/WebviewActivity.kt
+++ app/src/main/java/com/example/myapplication/WebviewActivity.kt
@@ -1,11 +1,15 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 
 class WebviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_webview)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.loadUrl("https://www.dicoding.com")
     }
 }
