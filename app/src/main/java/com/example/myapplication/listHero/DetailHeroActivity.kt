a5b5a3908aea4de2bf50797d3583557e9bd03f01
LAPTOP-LO1BRCPE\Lenovo <rayhannurikhsan.2008@gmail.com>
2026-09-14 17:04:04 +0700

    Hero Detail - get intent extra of hero data

--- app/src/main/java/com/example/myapplication/listHero/DetailHeroActivity.kt
+++ app/src/main/java/com/example/myapplication/listHero/DetailHeroActivity.kt
@@ -2,9 +2,18 @@ package com.example.myapplication.listHero
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityDetailHeroBinding
 
 class DetailHeroActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_NAME = "hero_name"
+        const val EXTRA_DESC = "hero_description"
+        const val EXTRA_PHOTO = "hero_photo"
+    }
 
     private lateinit var binding: ActivityDetailHeroBinding
 
@@ -12,5 +21,21 @@ class DetailHeroActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailHeroBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        displayHeroDetail()
+    }
+
+    private fun displayHeroDetail() {
+        binding.tvHeroDetailName.text = intent.getStringExtra(EXTRA_NAME)
+        binding.tvHeroDetailDesc.text = intent.getStringExtra(EXTRA_DESC)
+        Glide.with(this)
+            .load(intent.getStringExtra(EXTRA_PHOTO))
+            .apply(
+                RequestOptions()
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_launcher_foreground)
+                    .error(R.drawable.ic_launcher_foreground)
+            )
+            .into(binding.tvHeroDetailImage)
     }
 }
